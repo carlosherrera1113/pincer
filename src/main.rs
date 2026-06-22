c52dbf7bf5f37f88f5afbff3cf74045b8b40eae6
Carlos Herrera <carlitosherrera1113@gmail.com>
2026-06-22 15:58:41 -0700

    feat: tail MTG Arena logs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "/Users/carlosherrera/Library/Logs/Wizards Of The Coast/MTGA/Player.log";
+
+    let file = File::open(file_path).expect("Should open file.");
+    let mut reader = BufReader::new(file);
+
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        let num_bytes = reader.read_line(&mut line).expect("read error");
+
+        if num_bytes == 0 {
+            std::thread::sleep(std::time::Duration::from_millis(500));
+        }
+        println!("{line}");
+    }
 }
